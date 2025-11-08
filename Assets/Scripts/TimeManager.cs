072ed51757e711b0d8454c59c3f6312452ac6ec0
goocio <anguscb10@gmail.com>
2025-11-08 18:01:53 +0000

    time dilation added, need to choose rate of dilation

--- Assets/Scripts/TimeManager.cs
+++ Assets/Scripts/TimeManager.cs
@@ -9,6 +9,9 @@ public class TimeManager : MonoBehaviour
     List<GameObject> gravityObjects = new List<GameObject>();
     List<GameObject> movingPlatforms = new List<GameObject>();
 
+    int timeDilationStage = 0;
+    List<float> timeDilations = new List<float> { 1.0f, 0.95f, 0.9f, 0.85f, 0.8f, 0.75f, 0.7f, 0.65f, 0.6f, 0.5f, 0.4f, 0.35f, 0.3f, 0.25f, 0.1f, 0.05f, 0.0f };
+
     bool timeAlreadyStopped = true;
     List<Vector2> storedVelocities = new List<Vector2>();
 
@@ -40,19 +43,30 @@ public class TimeManager : MonoBehaviour
         }
     }
 
-    public void StopTime()
+    private void StopTimeDilated()
     {
-        for (int i = 0; i < gravityObjects.Count; i++)
+        bool timeJustStopped = timeDilationStage == 0;
+        if (timeDilationStage < timeDilations.Count - 1)
+            ++timeDilationStage;
+
+        for (int i = 0; i < gravityObjects.Count; ++i)
         {
             Rigidbody2D rb = gravityObjects[i].GetComponent<Rigidbody2D>();
             if (rb != null)
             {
-                rb.gravityScale = 0;
-                if (!timeAlreadyStopped) // at the moment time starts, preserve current momenta
+                if (timeJustStopped) // at the moment time stops, preserve current momenta
                     storedVelocities[i] = rb.linearVelocity;
-                rb.linearVelocity = Vector2.zero;
+
+                float dilation = timeDilations[timeDilationStage];
+                rb.gravityScale = dilation;
+                rb.linearVelocity = dilation * storedVelocities[i];
             }
         }
+    }
+
+    public void StopTime()
+    {
+        StopTimeDilated();
         timeAlreadyStopped = true;
         for (int i = 0; i < movingPlatforms.Count; i++)
         {
@@ -64,18 +78,27 @@ public class TimeManager : MonoBehaviour
         }
     }
 
-    public void StartTime()
+    private void StartTimeDilated()
     {
-        for (int i = 0; i < gravityObjects.Count; i++)
+        if (timeDilationStage == 0)
+            return;
+        --timeDilationStage;
+        for (int i = 0; i < gravityObjects.Count; ++i)
         {
             Rigidbody2D rb = gravityObjects[i].GetComponent<Rigidbody2D>();
             if (rb != null)
             {
-                rb.gravityScale = 1;
-                if (timeAlreadyStopped) // at the moment time restarts, restore previous momenta
-                    rb.linearVelocity = storedVelocities[i];
+                float dilation = timeDilations[timeDilationStage];
+
+                rb.gravityScale = dilation;
+                rb.linearVelocity = dilation * storedVelocities[i];
             }
         }
+    }
+
+    public void StartTime()
+    {
+        StartTimeDilated();
         timeAlreadyStopped = false;
         for (int i = 0; i < movingPlatforms.Count; i++)
         {
