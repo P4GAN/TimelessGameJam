d2aa8e15e41fa9651285286763a3a549dba237e8
brunomomp <bruno.momp.mar@gmail.com>
2025-11-09 15:14:14 +0000

    Menu and Level Select

--- Assets/Scripts/Events.cs
+++ Assets/Scripts/Events.cs
@@ -9,13 +9,45 @@ public class Events : MonoBehaviour
     {
         SceneManager.LoadScene("Menu");
     }
-    public void Levels()
-    {
-        SceneManager.LoadScene("Level Select");
-    }
     public void Level_1()
     {
         SceneManager.LoadScene("Level 1");
+    }
+    public void Level_2()
+    {
+        SceneManager.LoadScene("Level 2");
+    }
+    public void Level_3()
+    {
+        SceneManager.LoadScene("Level 3");
+    }
+    public void Level_4()
+    {
+        SceneManager.LoadScene("Level 4");
+    }
+    public void Level_5()
+    {
+        SceneManager.LoadScene("Level 5");
+    }
+    public void Level_6()
+    {
+        SceneManager.LoadScene("Level 6");
+    }
+    public void Level_7()
+    {
+        SceneManager.LoadScene("Level 7");
+    }
+    public void Level_8()
+    {
+        SceneManager.LoadScene("Level 8");
+    }
+    public void Level_9()
+    {
+        SceneManager.LoadScene("Level 9");
+    }
+    public void Level_10()
+    {
+        SceneManager.LoadScene("Final Level");
     }
     public void Quit()
     {
