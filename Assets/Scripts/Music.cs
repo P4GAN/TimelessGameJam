f9122bbe706c094ab10f1305ccf24ea9807ecf8d
P4GAN <parkinpham@gmail.com>
2025-11-09 22:58:38 +0000

    Fix

--- Assets/Scripts/Music.cs
+++ Assets/Scripts/Music.cs
@@ -2,10 +2,16 @@ using UnityEngine;
 
 public class MusicClass : MonoBehaviour
 {
+    public static MusicClass music;
     private AudioSource _audioSource;
     private void Awake()
     {
         DontDestroyOnLoad(transform.gameObject);
+        if (music == null) {
+		    music = this;
+        } else {
+            DestroyObject(gameObject);
+        }
         _audioSource = GetComponent<AudioSource>();
     }
 
