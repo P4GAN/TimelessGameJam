38381f63f7a3536562a413f5efbba2b4c6c54446
brunomomp <bruno.momp.mar@gmail.com>
2025-11-09 12:44:34 +0000

    3 Demo

---
+++ Assets/Scripts/SlowFallPlatform.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SlowFallPlatform : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float strength;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    private void Update()
+    {
+        rb.AddForce(Vector2.up * rb.gravityScale * strength, ForceMode2D.Force);
+    }
+    }
